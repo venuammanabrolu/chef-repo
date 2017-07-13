6b995fb119512905f2ef2cf2b021b640d30f2086
venugamma <venugamma@outlook.com>
2017-07-12 21:08:16 -0400

    Thirdd commit

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,13 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "venuammanabrolu"
-client_key               "#{current_dir}/venuammanabrolu.pem"
-chef_server_url          "https://chefserver-VirtualBox/organizations/testorgn"
+node_name                "venugamma1982"
+client_key               "#{current_dir}/venugamma1982.pem"
+validation_client_name	 "venu-testorgn-validator"
+validation_key		 "#{current_dir}/venu-testorgn-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/venu-testorgn"
+cache_type		 'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
